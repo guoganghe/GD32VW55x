a44cdeac5beb42789ad766d664d54d21dcd5e26a
guoganghe <178076107@qq.com>
2025-09-13 20:24:11 +0800

    user pdf

--- MSDK/app/_build_date.h
+++ MSDK/app/_build_date.h
@@ -1,2 +1,2 @@
 /* Do not change the content here, it's auto generated */ 
-#define SDK_BUILD_DATE "2025/08/12 22:45:32" 
+#define SDK_BUILD_DATE "2025/09/13 20:22:38" 
--- MSDK/projects/eclipse/msdk/msdk/plf/env/subdir.mk
+++ MSDK/projects/eclipse/msdk/msdk/plf/env/subdir.mk
@@ -4,12 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/env_init.c \
-D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/handlers.c 
+D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/env_init.c \
+D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/handlers.c 
 
 S_UPPER_SRCS += \
-D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/entry.S \
-D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/start.S 
+D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/entry.S \
+D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/start.S 
 
 C_DEPS += \
 ./plf/env/env_init.d \
@@ -23,28 +23,28 @@ OBJS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-plf/env/entry.o: D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/entry.S plf/env/subdir.mk
+plf/env/entry.o: D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/entry.S plf/env/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GD RISC-V MCU Assembler'
 	riscv-nuclei-elf-gcc -march=rv32imafcbp -mcmodel=medlow -msmall-data-limit=8 -msave-restore -mabi=ilp32f -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -fno-unroll-loops -Werror -Wunused -Wuninitialized -Wall -Wno-format -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable  -g -Wa,-adhlns=$@.lst   -x assembler-with-cpp -I"..\..\..\..\plf\riscv\NMSIS\Core\Include" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-plf/env/env_init.o: D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/env_init.c plf/env/subdir.mk
+plf/env/env_init.o: D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/env_init.c plf/env/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GD RISC-V MCU C Compiler'
 	riscv-nuclei-elf-gcc -march=rv32imafcbp -mcmodel=medlow -msmall-data-limit=8 -msave-restore -mabi=ilp32f -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -fno-unroll-loops -Werror -Wunused -Wuninitialized -Wall -Wno-format -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable  -g -std=c99 -DCFG_RTOS -DPLATFORM_OS_FREERTOS -I"..\..\..\..\lwip\iperf3" -I"..\..\..\..\lwip\iperf" -I"..\..\..\..\macsw\export" -I"..\..\..\..\macsw\import" -I"..\..\..\..\plf\riscv\arch" -I"..\..\..\..\plf\riscv\arch\boot" -I"..\..\..\..\plf\riscv\arch\lib" -I"..\..\..\..\plf\riscv\arch\ll" -I"..\..\..\..\plf\riscv\arch\compiler" -I"..\..\..\..\plf\src" -I"..\..\..\..\plf\src\reg" -I"..\..\..\..\plf\src\raw_flash" -I"..\..\..\..\plf\src\qspi_flash" -I"..\..\..\..\plf\src\dma" -I"..\..\..\..\plf\src\time" -I"..\..\..\..\plf\src\trng" -I"..\..\..\..\plf\src\uart" -I"..\..\..\..\plf\src\spi" -I"..\..\..\..\plf\src\spi_i2s" -I"..\..\..\..\plf\src\nvds" -I"..\..\..\..\plf\src\rf" -I"..\..\..\..\plf\riscv\gd32vw55x" -I"..\..\..\..\plf\riscv\NMSIS\Core\Include" -I"..\..\..\..\plf\riscv\NMSIS\DSP\Include" -I"..\..\..\..\plf\riscv\NMSIS\DSP\Include\dsp" -I"..\..\..\..\plf\GD32VW55x_standard_peripheral" -I"..\..\..\..\plf\GD32VW55x_standard_peripheral\Include" -I"..\..\..\..\rtos\rtos_wrapper" -I"..\..\..\..\rtos\FreeRTOS\Source\include" -I"..\..\..\..\rtos\FreeRTOS\Source\portable\riscv32" -I"..\..\..\..\rtos\FreeRTOS\config" -I"..\..\..\..\lwip\lwip-2.2.0\src\include" -I"..\..\..\..\lwip\lwip-2.2.0\src\include\compat\posix" -I"..\..\..\..\lwip\lwip-2.2.0\src\include\lwip" -I"..\..\..\..\lwip\lwip-2.2.0\src\include\lwip\apps" -I"..\..\..\..\lwip\lwip-2.2.0\port" -I"..\..\..\..\lwip\libcoap\include" -I"..\..\..\..\lwip\libcoap\port" -I"..\..\..\..\FatFS\port" -I"..\..\..\..\FatFS\src" -I"..\..\..\..\mbedtls\mbedtls-3.6.2\include" -I"..\..\..\..\mbedtls\mbedtls-3.6.2\library" -I"..\..\..\..\mbedtls\mbedtls-3.6.2\tests\include\spe" -I"..\..\..\..\..\ROM-EXPORT\bootloader" -I"..\..\..\..\..\ROM-EXPORT\halcomm" -I"..\..\..\..\..\ROM-EXPORT\symbol" -I"..\..\..\..\..\config" -I"..\..\..\..\app" -I"..\..\..\..\app\mqtt_app" -I"..\..\..\..\wifi_manager" -I"..\..\..\..\wifi_manager\wpas" -I"..\..\..\..\blesw\src\export" -I"..\..\..\..\ble\app" -I"..\..\..\..\ble\profile" -I"..\..\..\..\ble\profile\datatrans" -I"..\..\..\..\ble\profile\dis" -I"..\..\..\..\ble\profile\sample" -I"..\..\..\..\ble\profile\throughput" -I"..\..\..\..\ble\profile\bas" -I"..\..\..\..\ble\profile\ota" -I"..\..\..\..\util\include" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -Wa,-adhlns=$@.lst   -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-plf/env/handlers.o: D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/handlers.c plf/env/subdir.mk
+plf/env/handlers.o: D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/handlers.c plf/env/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GD RISC-V MCU C Compiler'
 	riscv-nuclei-elf-gcc -march=rv32imafcbp -mcmodel=medlow -msmall-data-limit=8 -msave-restore -mabi=ilp32f -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -fno-unroll-loops -Werror -Wunused -Wuninitialized -Wall -Wno-format -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable  -g -std=c99 -DCFG_RTOS -DPLATFORM_OS_FREERTOS -I"..\..\..\..\lwip\iperf3" -I"..\..\..\..\lwip\iperf" -I"..\..\..\..\macsw\export" -I"..\..\..\..\macsw\import" -I"..\..\..\..\plf\riscv\arch" -I"..\..\..\..\plf\riscv\arch\boot" -I"..\..\..\..\plf\riscv\arch\lib" -I"..\..\..\..\plf\riscv\arch\ll" -I"..\..\..\..\plf\riscv\arch\compiler" -I"..\..\..\..\plf\src" -I"..\..\..\..\plf\src\reg" -I"..\..\..\..\plf\src\raw_flash" -I"..\..\..\..\plf\src\qspi_flash" -I"..\..\..\..\plf\src\dma" -I"..\..\..\..\plf\src\time" -I"..\..\..\..\plf\src\trng" -I"..\..\..\..\plf\src\uart" -I"..\..\..\..\plf\src\spi" -I"..\..\..\..\plf\src\spi_i2s" -I"..\..\..\..\plf\src\nvds" -I"..\..\..\..\plf\src\rf" -I"..\..\..\..\plf\riscv\gd32vw55x" -I"..\..\..\..\plf\riscv\NMSIS\Core\Include" -I"..\..\..\..\plf\riscv\NMSIS\DSP\Include" -I"..\..\..\..\plf\riscv\NMSIS\DSP\Include\dsp" -I"..\..\..\..\plf\GD32VW55x_standard_peripheral" -I"..\..\..\..\plf\GD32VW55x_standard_peripheral\Include" -I"..\..\..\..\rtos\rtos_wrapper" -I"..\..\..\..\rtos\FreeRTOS\Source\include" -I"..\..\..\..\rtos\FreeRTOS\Source\portable\riscv32" -I"..\..\..\..\rtos\FreeRTOS\config" -I"..\..\..\..\lwip\lwip-2.2.0\src\include" -I"..\..\..\..\lwip\lwip-2.2.0\src\include\compat\posix" -I"..\..\..\..\lwip\lwip-2.2.0\src\include\lwip" -I"..\..\..\..\lwip\lwip-2.2.0\src\include\lwip\apps" -I"..\..\..\..\lwip\lwip-2.2.0\port" -I"..\..\..\..\lwip\libcoap\include" -I"..\..\..\..\lwip\libcoap\port" -I"..\..\..\..\FatFS\port" -I"..\..\..\..\FatFS\src" -I"..\..\..\..\mbedtls\mbedtls-3.6.2\include" -I"..\..\..\..\mbedtls\mbedtls-3.6.2\library" -I"..\..\..\..\mbedtls\mbedtls-3.6.2\tests\include\spe" -I"..\..\..\..\..\ROM-EXPORT\bootloader" -I"..\..\..\..\..\ROM-EXPORT\halcomm" -I"..\..\..\..\..\ROM-EXPORT\symbol" -I"..\..\..\..\..\config" -I"..\..\..\..\app" -I"..\..\..\..\app\mqtt_app" -I"..\..\..\..\wifi_manager" -I"..\..\..\..\wifi_manager\wpas" -I"..\..\..\..\blesw\src\export" -I"..\..\..\..\ble\app" -I"..\..\..\..\ble\profile" -I"..\..\..\..\ble\profile\datatrans" -I"..\..\..\..\ble\profile\dis" -I"..\..\..\..\ble\profile\sample" -I"..\..\..\..\ble\profile\throughput" -I"..\..\..\..\ble\profile\bas" -I"..\..\..\..\ble\profile\ota" -I"..\..\..\..\util\include" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -Wa,-adhlns=$@.lst   -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-plf/env/start.o: D:/work/103/github/GD32VW55x/MSDK/plf/riscv/env/start.S plf/env/subdir.mk
+plf/env/start.o: D:/work/WIFI_GD32VW/Github_GD32VW55x/MSDK/plf/riscv/env/start.S plf/env/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GD RISC-V MCU Assembler'
 	riscv-nuclei-elf-gcc -march=rv32imafcbp -mcmodel=medlow -msmall-data-limit=8 -msave-restore -mabi=ilp32f -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -fno-unroll-loops -Werror -Wunused -Wuninitialized -Wall -Wno-format -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable  -g -Wa,-adhlns=$@.lst   -x assembler-with-cpp -I"..\..\..\..\plf\riscv\NMSIS\Core\Include" -c -o "$@" "$<"
